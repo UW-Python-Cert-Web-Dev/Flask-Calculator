490bfcf44ee526c4ff4bcd219a5a6f24faedaee3
Joseph Schilz <joseph@schilz.org>
2017-10-02 22:27:27 -0700

    Adjust tests for calculator.

--- tests/web-tests.py
+++ tests/web-tests.py
@@ -10,24 +10,5 @@ class WebTests(unittest.TestCase):
 
     def testPageTitle(self):
         self.browser.get('http://localhost:5000/all')
-        self.assertIn('My Todo List', self.browser.title)
+        self.assertIn('Calculator', self.browser.title)
 
-    def testLogin(self):
-        self.browser.get('http://localhost:5000/all')
-
-        # Logout just incase
-        self.browser.get('http://localhost:5000/logout')
-
-        # Login button exists
-        self.assertIsNotNone(self.browser.find_element_by_css_selector('a[href="/login"]'))
-
-        # Click the login button
-        self.browser.find_element_by_css_selector('a[href="/login"]').click()
-
-        # Use the login form
-        self.browser.find_element_by_css_selector('input[name="name"]').send_keys("admin")
-        self.browser.find_element_by_css_selector('input[name="password"]').send_keys("password")
-        self.browser.find_element_by_css_selector('input[type="submit"]').click()
-
-        # We have successfully logged in and are at the "all tasks" page
-        self.assertIn("/all", self.browser.current_url)
